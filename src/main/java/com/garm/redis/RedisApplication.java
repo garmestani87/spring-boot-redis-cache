47c54fb4b2cad56bbce7108e428735ed49934433
mohsen <mohsen.salkhordeh66@gmail.com>
2023-07-10 16:54:15 +0330

    Update RedisApplication.java

--- src/main/java/com/garm/redis/RedisApplication.java
+++ src/main/java/com/garm/redis/RedisApplication.java
@@ -2,8 +2,10 @@ package com.garm.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class RedisApplication {
 
 	public static void main(String[] args) {
